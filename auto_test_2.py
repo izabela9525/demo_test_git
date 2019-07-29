725123465bfaae93506488781b8335c43dec3dca
@izabela.ciechanowskad <izabela.ciechanowskad@gmail.com>
2019-07-29 21:21:14 +0200

    Remove read me couse y don't need it now

--- auto_test_2.py
+++ auto_test_2.py
@@ -1,6 +1,27 @@
 import unittest
 from selenium import webdriver
 
-class MainTests:
+class MainTests(unittest.TestCase):
     def test_demo_login(self):
-        pass
+        driver = webdriver.Chrome(executable_path="C:\TestFiles\chromedriver.exe")
+        driver.get('https://demobank.jaktestowac.pl/logowanie_etap_1.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Logowanie' == title
+        driver.quit()
+
+    def test_demo_accounts(self):
+        driver = webdriver.Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
+        driver.get('https://demobank.jaktestowac.pl/konta.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Konta' == title
+        driver.quit()
+
+    def test_demo_pulpit(self):
+        driver = webdriver.Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
+        driver.get('https://demobank.jaktestowac.pl/pulpit.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Pulpit' == title
+        driver.quit()
