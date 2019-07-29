454680485c7ecdd31aaeb8c5de8d01639068c01b
@izabela.ciechanowskad <izabela.ciechanowskad@gmail.com>
2019-07-29 19:20:53 +0200

    First one

---
+++ auto_test_2.py
@@ -0,0 +1,6 @@
+import unittest
+from selenium import webdriver
+
+class MainTests:
+    def test_demo_login(self):
+        pass
--- demo_tests.py
+++ demo_tests.py
@@ -1,1 +1,8 @@
-print("Testing git 33jh6622")
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\TestFiles\chromedriver.exe")
+driver.get('https://demobank.jaktestowac.pl/logowanie_etap_1.html')
+title = driver.title
+print(title)
+assert 'Demobank - Bankowość Internetowa - Logowanie' == title
+driver.quit()
