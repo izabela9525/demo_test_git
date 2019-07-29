d49afef926ac8918445940404599a9a7e5ab27aa
@izabela.ciechanowskad <izabela.ciechanowskad@gmail.com>
2019-07-29 23:10:57 +0200

    new tests

---
+++ auto_test_2_refactor.py
@@ -0,0 +1,39 @@
+import unittest
+from selenium import webdriver
+
+class MainTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
+
+    def test_demo_login(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/logowanie_etap_1.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Logowanie' == title
+
+    def test_demo_accounts(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/konta.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Konta' == title
+
+    def test_demo_pulpit(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/pulpit.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Pulpit' == title
+
+    def test_demo_przelew(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/przelew_nowy_zew.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Przelew' == title
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
